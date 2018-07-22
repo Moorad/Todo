c4463fb8dafcb56d4bf43bb5c7b597ba5a2c3b1e
Moorad <34781684+mooradal@users.noreply.github.com>
2018-07-22 23:27:05 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,58 @@
-var button = document.getElementById("add_button")
+var button = document.getElementById("plus")
+var new_element_box = document.getElementById("new_element_container")
+var menu = document.getElementById("menu")
+var title_input = document.getElementById("title_input")
+var description_input = document.getElementById("description_input")
 var adding_window = false;
 
 function add() {
   if (!adding_window) {
   button.style.transform = "rotate(45deg)"
   adding_window =  true
+  new_element_box.hidden = false
 } else if (adding_window) {
   button.style.transform = "rotate(180deg)"
   adding_window = false
-}
+  new_element_box.hidden = true
+  }
 }
 
-create_elements();
+function create_element() {
+  new_element_box.hidden = true
+  adding_window = false
+  element_container = document.createElement("div")
+  element_container.className = "element_container"
 
-function create_elements() {
-  var container = document.createElement("div")
-  var tickbox = document.createElement("div")
-  var text = document.createElement("div")
-  container.class = "container";
-  tickbox.class = "tickbox";
-  text.class = "todo-text";
+  checkbox = document.createElement("div")
+  checkbox.className = "checkbox"
+  checkbox.onclick = function() {checkbox_clicked(this)}
+  img = document.createElement("img")
+  img.src = "Assets/tick.png"
+  img.hidden = true
+  checkbox.appendChild(img)
+
+  title = document.createElement("h1")
+  title.className = "title"
+  title.innerHTML = title_input.value
+
+  description = document.createElement("p")
+  description.className = "description"
+  description.innerHTML = description_input.value
+
+  text_container = document.createElement("div")
+  text_container.className = "text_container"
+  text_container.appendChild(title)
+  text_container.appendChild(description)
+
+  element_container.appendChild(checkbox)
+  element_container.appendChild(text_container)
+  menu.appendChild(element_container)
+}
+
+function checkbox_clicked(element) {
+  if (element.style.backgroundColor == "rgb(0, 0, 0)") {
+  element.style.backgroundColor = "#FFFFFF"
+} else {
+  element.style.backgroundColor = "#000000"
+}
 }
